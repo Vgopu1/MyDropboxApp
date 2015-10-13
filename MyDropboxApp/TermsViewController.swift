7d2f603457f1f10e85431adbecfdc5692ac63bf9
Veena <vgopu0@walmart.com>
2015-10-13 03:57:14 -0700

    Dropbox App
    Dropbox App

---
+++ MyDropboxApp/TermsViewController.swift
@@ -0,0 +1,41 @@
+//
+//  TermsViewController.swift
+//  MyDropboxApp
+//
+//  Created by Veena on 10/13/15.
+//  Copyright © 2015 Veena. All rights reserved.
+//
+
+import UIKit
+
+class TermsViewController: UIViewController {
+
+    // @IBOutlet weak var onButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    //@IBAction func onButton(sender: AnyObject) {
+        
+        // navigationController!.popViewControllerAnimated(true)
+        //navigationController?.popViewControllerAnimated(true)
+    //}
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
